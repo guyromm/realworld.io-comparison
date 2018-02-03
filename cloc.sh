c1dcc81ad0c9204d64c7f04aaed078d540eb16df
Guy Romm <guyromm@guyromm.com>
2018-02-03 14:48:49 +0200

    wip

--- cloc.sh
+++ cloc.sh
@@ -3,6 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 rm -rf $DIR/results/*txt
 for D in $(find ./apps -maxdepth 1 -type d ! -iname '.' ! -iname 'apps') ; do
     echo "********** $D"
-    find $DIR/$D -type f ! -wholename '*node_modules/*' ! -wholename '*TestCase/*' ! -wholename '*/tests/*' | xargs cloc > results/$(basename $D)".txt" ;
+    find $DIR/$D -type f ! -wholename '*node_modules/*' ! -wholename '*TestCase/*' ! -wholename '*/tests/*' | xargs cloc > results/$(basename $D)".txt" && \
+	sed -i -E 's/T=(.*)$//g' results/$(basename $D)".txt"
 done
 grep SUM results/*txt | awk '{print $5,$1}' | sort -k1n
