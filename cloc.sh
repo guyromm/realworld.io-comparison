93b7c46fc4f25230f97ccadb24db4d5c3f7e7b86
Guy Romm <guyromm@guyromm.com>
2018-02-03 14:45:21 +0200

    initial

---
+++ cloc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+rm -rf $DIR/results/*txt
+for D in $(find ./apps -maxdepth 1 -type d ! -iname '.' ! -iname 'apps') ; do
+    echo "********** $D"
+    find $DIR/$D -type f ! -wholename '*node_modules/*' ! -wholename '*TestCase/*' ! -wholename '*/tests/*' | xargs cloc > results/$(basename $D)".txt" ;
+done
+grep SUM results/*txt | awk '{print $5,$1}' | sort -k1n
