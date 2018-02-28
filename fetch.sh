326d6a5ce71ff1176354ab43e201b5d6625b8763
Guy Romm <guyromm@gmail.com>
2018-02-28 19:38:53 +0200

    switched to submodules.

--- fetch.sh
+++ fetch.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 function fetch_repos() {
-TDIR=$1'-apps/'
+TDIR=$DIR"/"$1'-apps/'
 mkdir -p $TDIR
+
 for RURL in $(cat $1-side-repos.txt) ; do
+    cd $TDIR
     BN="$(basename $RURL)"
-    git clone $RURL $TDIR$BN
+    git submodule add $RURL # $TDIR$BN
     done
 }
 fetch_repos $1
+cd $DIR
